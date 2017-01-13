fec320a0ee172887ccd6511fa0ad03036d76d4d7
Endre <macbookpro@MacBookPro-MacBook-Pro.local>
2017-01-13 09:25:58 +0100

    React testing

---
+++ src/App.js
@@ -0,0 +1,486 @@
+import React from 'react';
+
+// class App extends React.Component {
+//   render() {
+//     return React.createElement('h1', null, 'Hello Eggheads');
+//   }
+// }
+
+
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello Series</h1>
+//         <b>Bold</b>
+//       </div>
+//     )
+//   }
+// }
+
+// const App = () => <h1>Hello stateless</h1>
+
+
+
+
+// class App extends React.Component {
+//   render() {
+//     let txt = this.props.txt
+//     return <h1>{txt}</h1>
+//   }
+// }
+//
+// App.propTypes = {
+//   txt: React.PropTypes.string,
+//   cat: React.PropTypes.number.isRequired
+// }
+//
+// App.defaultProps = {
+//   txt: "this is the default text"
+// }
+
+
+
+//************** MANAGE REACT COMPONENT STATE WITH setState ***************
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       txt: 'this is the state txt',
+//       cat: 0
+//     }
+//   }
+//   update( e ) {
+//     this.setState({txt: e.target.value})
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <input type="text"
+//         onChange={this.update.bind(this)} />
+//         <h1>{this.state.txt} - {this.state.cat}</h1>
+//       </div>
+//     )
+//
+//   }
+// }
+
+
+
+
+//************** USE REACT COMPONENTS AS CHILDREN FOR OTHER COMPONENTS *****
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       txt: 'this is the state txt'
+//     }
+//   }
+//   update(e) {
+//     this.setState({txt: e.target.value})
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.txt}</h1>
+//         <Widget update={this.update.bind(this)} />
+//         <Widget update={this.update.bind(this)} />
+//         <Widget update={this.update.bind(this)} />
+//       </div>
+//     )
+//   }
+// }
+//
+// const Widget = (props) =>
+//   <input type='text' onChange={props.update} />
+
+
+
+
+//************** ACCES NESTED DATA WITH REACTS props.children **************
+// class App extends React.Component {
+//   render() {
+//     return <Button>I <Heart /> React</Button>
+//   }
+// }
+//
+// const Button = (props) => <button>{props.children}</button>
+//
+// class Heart extends React.Component {
+//   render() {
+//     return <span>&hearts;</span>
+//   }
+// }
+
+
+
+//************** ADD CUSTOM PROTOTYPE VALIDATION TO REACT COMPONENTS *******
+// class App extends React.Component {
+//   render() {
+//     return <Title text="123456" />
+//   }
+// }
+//
+// const Title = (props) => <h1>Title: {props.text}</h1>
+//
+// Title.propTypes = {
+//   text(props, propName, component) {
+//     if(!(propName in props)) {
+//       return new Error(`missing ${propName}`)
+//     }
+//     if (props[propName].length < 6) {
+//       return new Error(`${propName} was too short`)
+//     }
+//   }
+// }
+
+
+
+//************** NORMALIZE EVENTS WITH REACTS SYNTHETIC EVENT SYSTEM *******
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {currentEvent: '---'}
+//     this.update = this.update.bind(this)
+//   }
+//   update(e) {
+//     this.setState({currentEvent: e.type})
+//   }
+//   render(){
+//     return (
+//       <div>
+//         <textarea
+//           onKeyPress={this.update}
+//           onCopy={this.update}
+//           onCut={this.update}
+//           onPaste={this.update}
+//           onFocus={this.update}
+//           onBlur={this.update}
+//           onDoubleClick={this.update}
+//           onTouchStart={this.update}
+//           onTouchMove={this.update}
+//           onTouchEnd={this.update}
+//           cols="30"
+//           rows="10" />
+//         <h1>{this.state.currentEvent}</h1>
+//       </div>
+//     )
+//   }
+// }
+
+
+
+//************** USE REACT REF TO GET A REFERENCE TO SPECIFIC COMPONENTS ***
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {a: ''}
+//   }
+//   update() {
+//     this.setState({
+//       a: this.a.refs.input.value,
+//       b: this.refs.b.value
+//     })
+//   }
+//   render(){
+//     return (
+//       <div>
+//         <Input
+//           ref={ component => this.a = component}
+//           update={this.update.bind(this)}
+//         /> {this.state.a}
+//         <hr />
+//         <input
+//           ref="b"
+//           type="text"
+//           onChange={this.update.bind(this)}
+//         /> {this.state.b}
+//       </div>
+//     )
+//   }
+// }
+//
+// class Input extends React.Component {
+//   render() {
+//     return <div><input ref="input" type="text" onChange={this.props.update} /></div>
+//   }
+// }
+
+
+
+//********** UNDERSTAND/MANAGE REACT COMPONENT STATE w/ LIFECYCLE METHODS ***
+// import ReactDOM from 'react-dom';
+//
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {val: 0}
+//     this.update = this.update.bind(this)
+//   }
+//   update() {
+//     this.setState({val: this.state.val + 1})
+//   }
+//   componentWillMount() {
+//     console.log('componentWillMount')
+//     this.setState({m: 2})
+//   }
+//   render(){
+//     console.log('render');
+//     return <button onClick={this.update}>
+//         {this.state.val * this.state.m}
+//       </button>
+//   }
+//   componentDidMount() {
+//     console.log('componentDidMount');
+//     this.inc = setInterval(this.update, 500)
+//   }
+//   componentWillUnmount() {
+//     console.log('componentWillUnmount');
+//     clearInterval(this.inc)
+//   }
+// }
+//
+// class Wrapper extends React.Component {
+//   mount() {
+//     ReactDOM.render(<App />, document.getElementById('a'))
+//   }
+//   unmount() {
+//     ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.mount.bind(this)}>Mount</button>
+//         <button onClick={this.unmount.bind(this)}>UnMount</button>
+//         <div id="a"></div>
+//       </div>
+//     )
+//   }
+// }
+//
+// export default Wrapper
+
+
+
+
+//***** CONTROL REACT COMPONENT UPDATES WHEN NEW PROPS ARE RECEIVED ********
+// import ReactDOM from 'react-dom';
+//
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {increasing: false}
+//   }
+//   update(){
+//     ReactDOM.render(
+//       <App val={this.props.val + 1} />,
+//       document.getElementById('root'))
+//   }
+//   componentWillReceiveProps(nextProps){
+//     this.setState({increasing: nextProps.val > this.props.val})
+//   }
+//   shouldComponentUpdate(nextProps, nextState) {
+//     return nextProps.val % 5 === 0;
+//   }
+//   render() {
+//     console.log(this.state.increasing);
+//     return (
+//       <button onClick={this.update.bind(this)}>
+//         {this.props.val}
+//       </button>
+//     )
+//   }
+//   componentDidUpdate(prevProps, prevState) {
+//     console.log(`prevProps: ${prevProps.val}`);
+//   }
+// }
+//
+// App.defaultProps = {val: 0}
+
+
+
+
+// ***** USE MAP TO CREATE REACT COMPONENTS FROM ARRAYS OF DATA ********
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {items: []}
+//   }
+//   componentWillMount(){
+//     fetch( 'http://swapi.co/api/people?format=json' )
+//       .then( response => response.json() )
+//       .then( ({results: items}) => this.setState({items}))
+//   }
+//   filter(e) {
+//     this.setState({filter: e.target.value})
+//   }
+//   render() {
+//     let items = this.state.items;
+//     if(this.state.filter){
+//       items = items.filter( item =>
+//         item.name.toLowerCase()
+//         .includes(this.state.filter.toLowerCase()))
+//     }
+//     return (
+//       <div>
+//         <input type="text"
+//         onChange={this.filter.bind(this)}/>
+//         {items.map(item =>
+//           <Person key={item.name} person={item} />)}
+//       </div>
+//     )
+//   }
+// }
+//
+// const Person = (props) => <h4>{props.person.name}</h4>
+
+
+
+
+//***** COMPOSE REACT COMPONENT BEHAVIOR WITH HIGHER ORDER COMPONENTS ********
+// const HOC = (InnerComponent) => class extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {count: 0}
+//   }
+//   update() {
+//     this.setState({count: this.state.count + 1})
+//   }
+//   componentWillMount() {
+//     console.log('will mount');
+//   }
+//
+//   render() {
+//     return (
+//       <InnerComponent
+//         {...this.props}
+//         {...this.state}
+//         update={this.update.bind(this)}
+//       />
+//     )
+//   }
+// }
+//
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Button>button</Button>
+//         <hr />
+//         <LabelHOC>label</LabelHOC>
+//       </div>
+//     )
+//   }
+// }
+//
+// const Button = HOC((props) =>
+//   <button onClick={props.update}>{props.children} - {props.count}</button>
+// )
+//
+// class Label extends React.Component {
+//   componentWillMount() {
+//     console.log('label will mount');
+//   }
+//   render() {
+//     return (
+//       <label onMouseMove={this.props.update}>
+//       {this.props.children} - {this.props.count}
+//       </label>
+//     )
+//   }
+// }
+//
+// const LabelHOC = HOC(Label)
+
+
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       input: '/* add your jsx here */',
+//       output: '',
+//       err: ''
+//     }
+//   }
+//   update(e) {
+//     let code = e.target.value;
+//     try {
+//       this.setState({
+//         output: window.Babel
+//         .transform(code, { presets: ['es2015', 'react']})
+//         .code,
+//         err: ''
+//       })
+//     }
+//     catch(err) {
+//       this.setState({err: err.message})
+//     }
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <header>{this.state.err}</header>
+//         <div className="container">
+//           <textarea
+//           onChange={this.update.bind(this)}
+//           defaultValue={this.state.input} />
+//           <pre>
+//           {this.state.output}
+//           </pre>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+
+// ReactDOM.render(
+//   <App cat={5} />,
+//   document.getElementById('root')
+// );
+
+//************** H1 CONTENT CHANGES WITH THE DIV *******************
+//************** MORE INPUT CAN UPDATE THE SAME DIV *******************
+//************** SPAN INSIDE BUTTON TEXT *******************
+//************** CUSTOM VALIDATION *******************
+//************** UPDATE DIV WITH TYPE(KIND) OF EVENTS *******************
+//************** UPDATE 2 DIFFERENT COMPONENTS IN 2 DIFFERENT WAYS ****
+//************** BUTTON EVENTS, COUNTER, COMPLICATED STUFFS ********
+//***** CONTROL REACT COMPONENT UPDATES WHEN NEW PROPS ARE RECEIVED ********
+//***** USE MAP TO CREATE REACT COMPONENTS FROM ARRAYS OF DATA ********
+//***** COMPOSE REACT COMPONENT BEHAVIOR WITH HIGHER ORDER COMPONENTS ********
+ReactDOM.render(
+  <App cat={5} />,
+  document.getElementById('root')
+);
